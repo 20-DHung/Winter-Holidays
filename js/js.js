27b9a7c7a2a1cc12034d4247e821c17f7371257e
20-DHung <duyhung240602@gmail.com>
2022-06-20 21:38:00 +0700

    xu ly responsive

--- js/js.js
+++ js/js.js
@@ -15,3 +15,20 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+
+
+// Responsive
+const navMobileBtn = document.querySelector('.js-mobile-btn')
+const mobileMenu = document.querySelector('.js-mobile-menu')
+var mobileOverlay = document.querySelector('.mobile-menu-overlay')
+
+navMobileBtn.onclick=function(){
+    mobileMenu.style.transform="scaleY(1)";
+    mobileOverlay.style.display="block";
+}
+
+mobileOverlay.onclick=function(){
+    mobileMenu.style.transform="scaleY(0)";
+    mobileOverlay.style.display="none";
+}
